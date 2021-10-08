e44efbf2ff4ba1388ca3c82973f0cc48a0ddabf1
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 02:53:20 +0530

    Initial commit

---
+++ main.sh
@@ -0,0 +1,15 @@
+First Install Homebrew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+Second
+
+sudo gem install cocoapods
+
+Third
+
+pod setup
+
+Fourth
+
+pod install
